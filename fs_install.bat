fbb0ca5424a7577aa0cecfb289f1d8dad7338f29
Janki Gergő <65868492+AthoxGit@users.noreply.github.com>
2021-02-06 19:43:40 +0100

    Update fs_install.bat

--- fs_install.bat
+++ fs_install.bat
@@ -1,5 +1,5 @@
 @Echo off
-Title Installing FS
+Title FS - Fail
 npm i fs 
-Echo FS installed. You can close this window, and start gen.bat.
+Echo FS is not installed. Install Node.JS: https://nodejs.org/dist/v14.15.4/node-v14.15.4-x64.msi , And try again.
 pause
